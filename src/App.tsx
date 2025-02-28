c43ea5d33bf90e1ac40e4a0abf7b68e82db7c8e8
MRnektodotpng <android.kaplan@gmail.com>
2025-02-28 02:56:05 +0100

    added all planets

--- src/App.tsx
+++ src/App.tsx
@@ -38,14 +38,14 @@ const Earth = ({pos, scale = [1,1,1 ]}:{pos:[number,number,number], scale?:[numb
   const cloudsRef = useRef<THREE.Mesh>(null!)
 
   const textures = useTexture({
-    map: "src/assets/Albedo.jpg",           
-    specularMap: "src/assets/8081_earthspec4k.jpg", 
-    bumpMap: "src/assets/8081_earthbump4k.jpg",     
-    normalMap: "src/assets/8k_earth_normal_map.jpg", 
-    emissiveMap: "src/assets/8k_earth_nightmap.jpg"  
+    map: "src/assets/Albedo.webp",           
+    specularMap: "src/assets/8081_earthspec4k.webp", 
+    bumpMap: "src/assets/8081_earthbump4k.webp",     
+    normalMap: "src/assets/8k_earth_normal_map.webp", 
+    emissiveMap: "src/assets/8k_earth_nightmap.webp"  
   })
 
-  const cloudTexture = useTexture("src/assets/8k_earth_clouds.jpg")
+  const cloudTexture = useTexture("src/assets/8k_earth_clouds.webp")
 
 
   useFrame((state,delta) => {
@@ -147,7 +147,7 @@ const Mercury = ({pos}:{pos:[number,number,number]}) => {
   const mercuryRef = useRef<THREE.Group>(null!)
 
   const texture = useTexture({
-    map:"src/assets/8k_mercury.jpg",
+    map:"src/assets/8k_mercury.webp",
   })
 
   return (
@@ -168,7 +168,7 @@ const Venus = ({pos}:{pos:[number,number,number]}) => {
   const venusRef = useRef<THREE.Group>(null!)
 
   const textures = useTexture({
-    map:"src/assets/8k_venus_surface.jpg"
+    map:"src/assets/8k_venus_surface.webp"
   })
 
   return (
@@ -176,6 +176,106 @@ const Venus = ({pos}:{pos:[number,number,number]}) => {
       <mesh>
         <sphereGeometry args={[2,32,32]} />
         <meshStandardMaterial {...textures}/>
+      </mesh>
+    </group>
+  )
+}
+
+
+const Mars = ({pos}:{pos:[number,number,number]}) => {
+  const marsRef = useRef<THREE.Group>(null!)
+
+  const textures = useTexture({
+    map:"src/assets/8k_mars.webp",
+    normalMap:"src/assets/mars_1k_normal.jpg",
+    
+  })
+
+  return (
+    <group position={pos} ref={marsRef}>
+      <mesh>
+        <sphereGeometry args={[2,32,32]} />
+        <meshStandardMaterial {...textures }/>
+      </mesh>
+    </group>
+  )
+}
+
+const Jupiter = ({pos}:{pos:[number,number,number]}) => {
+  const jupiterRef = useRef<THREE.Group>(null!)
+
+  const textures = useTexture({
+    map:"src/assets/8k_jupiter.webp",
+   
+    
+  })
+
+  return (
+    <group position={pos} ref={jupiterRef}>
+      <mesh>
+        <sphereGeometry args={[2,32,32]} />
+        <meshStandardMaterial {...textures }/>
+      </mesh>
+    </group>
+  )
+}
+
+const Saturn = ({pos}:{pos:[number,number,number]}) => {
+  const saturnRef = useRef<THREE.Group>(null!)
+
+  const textures = useTexture({
+    map:"src/assets/8k_saturn.webp",
+
+   
+    
+  })
+
+  return (
+    <group position={pos} ref={saturnRef}>
+      <mesh>
+        <sphereGeometry args={[2,32,32]} />
+        <meshStandardMaterial {...textures }/>
+      </mesh>
+    </group>
+  )
+}
+
+const Uranus = ({pos}:{pos:[number,number,number]}) => {
+  const saturnRef = useRef<THREE.Group>(null!)
+
+  const textures = useTexture({
+    map:"src/assets/2k_uranus.webp",
+
+   
+    
+  })
+
+  return (
+    <group position={pos} ref={saturnRef}>
+      <mesh>
+        <sphereGeometry args={[2,32,32]} />
+        <meshStandardMaterial {...textures }/>
+      </mesh>
+    </group>
+  )
+}
+
+
+const Neptune = ({pos}:{pos:[number,number,number]}) => {
+  const saturnRef = useRef<THREE.Group>(null!)
+
+  const textures = useTexture({
+    map:"src/assets/2k_neptune.webp",
+
+   
+    
+  })
+
+  return (
+    <group position={pos} ref={saturnRef}>
+      <mesh>
+        <sphereGeometry args={[2,32,32]} />
+        <meshStandardMaterial {...textures }/>
       </mesh>
     </group>
   )
@@ -202,9 +302,14 @@ const Space = () => {
     return (
       <Canvas camera={{position:[0,20,30]}}>
         <Skybox/>
-        <Earth pos={[0,0,0]}/>
         <Mercury pos={[25,0,0]}/>
         <Venus pos={[0,0,25]}/>
+        <Earth pos={[0,0,0]}/>
+        <Mars pos={[0,5,25]}/>
+        <Jupiter pos={[25,5,5]}/>
+        <Saturn pos={[12,15,5]}/>
+        <Uranus pos={[25,25,25]}/>
+        <Neptune pos={[10,5,25]}/>
         <Moon earthPosition={[0, 0, 0]} orbitRadius={12} orbitSpeed={0.2}/>
         <pointLight position={[30,30,30]} intensity={100 } decay={1}/> 
         <axesHelper args={[10]}/>
